0b272f5af3946a72a027f5ca714de702162310c0
yuyuaqwq <1454832774@qq.com>
2023-09-10 23:41:12 +0800

    序列化完事

--- sezz.hpp
+++ sezz.hpp
@@ -37,6 +37,13 @@ struct is_string_t : std::false_type {};
 template<typename T>
 struct is_string_t<std::basic_string<T>> : std::true_type {};
 
+/* 判断是否是std::vector */
+template<typename T>
+struct is_vector_t : std::false_type {};
+template<typename T>
+struct is_vector_t<std::vector<T>> : std::true_type {};
+
+
 /* 判断是否可迭代(type版) */
 template<typename T, typename = void>
 struct is_iterable_t : std::false_type {};
@@ -62,47 +69,37 @@ constexpr bool is_string_v = details::is_string_t<T>::value;
 /* 判断是否是std::pair(value版) */
 template <class T>
 constexpr bool is_pair_v = details::is_pair_t<T>::value;
+/* 判断是否是std::vector(value版) */
+template <class T>
+constexpr bool is_vector_v = details::is_vector_t<T>::value;
 
-//  可平凡复制
-template <class T, typename std::enable_if_t<std::is_trivially_copyable_v<T>, int> N = 0>
-void Serialize(std::ostream& os, const T& val) {
-    os.write((const char*)&val, sizeof(T));
-}
-
-//  容器
-
-template <class T, typename std::enable_if_t<
-    std::is_same_v<typename T::iterator, decltype(std::declval<T>().begin())>&&
-    std::is_same_v<typename T::iterator, decltype(std::declval<T>().end())>&&
-    std::is_trivially_copyable_v<typename T::value_type>, int> N = 0>
-void Serialize(std::ostream& os, const T& val) {
-    unsigned int size = val.size();
-    os.write((const char*)&size, sizeof(size));
-    os.write((const char*)val.data(), size * sizeof(typename T::value_type));
-}
-
-template <class T1, class T2>
-void Serialize(std::ostream& os, const std::pair<T1, T2>& val);
-
-template <class T, typename std::enable_if_t<
-    std::is_same_v<typename T::iterator, decltype(std::declval<T>().begin())>&&
-    std::is_same_v<typename T::iterator, decltype(std::declval<T>().end())> &&
-    !std::is_trivially_copyable_v<typename T::value_type>, int> N = 0>
-void Serialize(std::ostream& os, const T& val) {
-    unsigned int size = val.size();
-    os.write((const char*)&size, sizeof(size));
-    for (auto& v : val) { Serialize(os, v); }
-}
-
-// 对组支持
-template <class T1, class T2>
-void Serialize(std::ostream& os, const std::pair<T1, T2>& val) {
-    Serialize(os, val.first);
-    Serialize(os, val.second);
+template <class T>
+void Serialize(std::ostream& os, T& val) {
+    using DecayT = std::decay_t<T>;
+    if constexpr (is_pair_v<DecayT>) {
+        Serialize(os, val.first);
+        Serialize(os, val.second);
+    }
+    else if constexpr (is_iterable_v<DecayT>) {
+        unsigned int size = val.size();
+        os.write((const char*)&size, sizeof(size));
+        if constexpr (is_memcopyable_v<DecayT>) {
+            os.write((const char*)&val, size * sizeof(typename T::value_type));
+        }
+        else {
+            for (auto& v : val) { Serialize(os, v); }
+        }
+    }
+    else if constexpr (is_memcopyable_v<T>) {
+        os.write((const char*)&val, sizeof(T));
+    }
+    else {
+        //printf("无法解析的T类型: %s\n", typeid(T).name()); throw;
+        static_assert(always_false<T>, "无法解析的T类型!");
+    }
 }
 
 
-//  可平凡复制
 template <class T>
 T Deserialize(std::istream& is) {
     using DecayT = std::decay_t<T>;
@@ -131,14 +128,22 @@ T Deserialize(std::istream& is) {
         */
         else {
             is.read((char*)&size, sizeof(unsigned int));
+            if constexpr (is_vector_v<DecayT>) {
+                res.resize(size);
+            }
             for (unsigned int i = 0; i < size; i++) {
                 auto tmp = Deserialize<typename T::value_type>(is);     // 反序列化T的元素, 比如std::string
                 /* 如果是std::basic_string */
-                if constexpr (is_string_v<DecayT>)
-                    res.insert(i, 1, tmp);
+                if constexpr (is_string_v<DecayT>) {
+                    res.insert(i, 1, std::move(tmp));
+                }
                 /* 如果是std::unordered_map */
-                else if constexpr (is_unordered_map_v<DecayT>)
-                    res.insert(tmp);
+                else if constexpr (is_unordered_map_v<DecayT>) {
+                    res.insert(std::move(tmp));
+                }
+                else if constexpr (is_vector_v<DecayT>) {
+                    res[i] = std::move(tmp);
+                }
                 /* 剩下的交给你了! 注释的printf是可以在运行时查看还有什么类型没实现 */
                 else {
                     //printf("T是还没有实现的容器: %s\n", typeid(DecayT).name()); throw;
@@ -157,6 +162,8 @@ T Deserialize(std::istream& is) {
     }
     return res;
 }
+
+
 
 } // namespace sezz
 
